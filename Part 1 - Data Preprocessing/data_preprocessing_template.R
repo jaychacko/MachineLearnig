4eccee59c27b6437ed2e0f3c1277817e724a4c44
jaisil007 <jaisilchacko@yahoo.com>
2017-09-05 00:23:36 -0500

    data preprocess template

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,16 +1,20 @@
-# Data Preprocessing Template
+# Data Preprocessing 
 
-# Importing the dataset
+#import dataset
 dataset = read.csv('Data.csv')
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+#missing value fix
+#age
+dataset$Age = ifelse(is.na(dataset$Age),ave(
+  dataset$Age,FUN = function(x)mean(x,na.rm = TRUE)),
+  dataset$Age)
+#salary
+dataset$Salary = ifelse(is.na(dataset$Salary),ave(
+  dataset$Salary,FUN = function(x)mean(x,na.rm = TRUE)),
+  dataset$Salary)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#encoding categorical data
+dataset$Country = factor(dataset$Country,levels = c('France','Spain','Germany')
+                         ,labels = c(1,2,3))
+dataset$Purchased = factor(dataset$Purchased,levels = c('No','Yes')
+                         ,labels = c(0,1))
--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,23 +1,53 @@
 # Data Preprocessing Template
 
 # Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#importing dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+x = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,3].values
+
+# fixing missing values
+
+from sklearn.preprocessing import Imputer
+
+# nan is how table shows no data, we are using mean to replace
+imputer = Imputer(missing_values="NaN",strategy='mean',axis=0)
+# we are targetting only missing columsn which are only in index 2 and 3
+#python indedx 1 and 3
+imputer = imputer.fit(x[:,1:3])
+x[:,1:3] =imputer.transform(x[:,1:3] )
+
+
+#encoding categorical data
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+
+labelencoder_x = LabelEncoder()
+x[:,0] = labelencoder_x.fit_transform(x[:,0])
+
+#onehot dummy variable
+onehotencoder = OneHotEncoder(categorical_features = [0])
+x=onehotencoder.fit_transform(x).toarray()
+
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
+
+#spliting into traiing set and test set 
+
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2,
+                                                 random_state=0)
+
+# feature scaling
+
+from sklearn.preprocessing import StandardScaler
+sc_x=StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test = sc_x.transform(x_test)
