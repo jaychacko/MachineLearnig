db7cbac5f76bd84aeedea900c708b42863dab251
jaisil007 <jaisilchacko@yahoo.com>
2017-09-06 19:37:23 -0500

    simple linear

--- Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -18,3 +18,38 @@ dataset$Country = factor(dataset$Country,levels = c('France','Spain','Germany')
                          ,labels = c(1,2,3))
 dataset$Purchased = factor(dataset$Purchased,levels = c('No','Yes')
                          ,labels = c(0,1))
+
+#splitting test and training
+#install.packages('caTools')
+
+library(caTools)
+
+set.seed(123)
+split = sample.split(dataset$Purchased,SplitRatio = 0.8)
+trainig_set  =subset(dataset,split ==TRUE)
+test_set  =subset(dataset,split ==FALSE)
+
+
+#featurscaling
+#excluding 2 and 3 column since they are not numbers since we only foctored it 
+
+trainig_set [,2:3]= scale(trainig_set[,2:3])
+test_set[,2:3] = scale(test_set[,2:3])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
