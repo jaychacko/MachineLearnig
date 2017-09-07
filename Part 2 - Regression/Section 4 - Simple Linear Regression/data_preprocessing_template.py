803e2fd817cb512111fce1e2b6592577fe7620af
jaisil007 <jaisilchacko@yahoo.com>
2017-09-06 19:39:27 -0500

    simple linear

--- Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.py
@@ -21,3 +21,26 @@ from sklearn.linear_model import LinearRegression
 
 regressor = LinearRegression()
 regressor.fit(X_train,y_train)
+
+#predicting the test set 
+
+y_pred = regressor.predict(X_test)
+
+#visualise data of trainig set results
+
+#pyplot function
+plt.scatter(X_train,y_train, color = "red")
+y_predReal = regressor.predict(X_train)
+plt.plot(X_train,y_predReal,color="blue")
+plt.title('Salary vs Experiance (Training set)')
+plt.xlabel('Years of Experiance')
+plt.ylabel('Salary')
+plt.show()
+
+#visualizing test set results
+plt.scatter(X_test,y_test, color = "red")
+plt.plot(X_train,y_predReal ,color="blue")
+plt.title('Salary vs Experiance (Training set)')
+plt.xlabel('Years of Experiance')
+plt.ylabel('Salary')
+plt.show()
